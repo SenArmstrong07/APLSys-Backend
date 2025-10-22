3c985bce96f4235b52d61d1dd73bcde40c27156c
SenArmstrong07 <108717496+SenArmstrong07@users.noreply.github.com>
2025-10-22 11:12:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def root():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uv.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uv.run("main:app", host="0.0.0.0", port=$PORT, reload=True)
