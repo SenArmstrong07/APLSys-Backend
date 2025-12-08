62666fc7c5098f9f937b1035bb63401a6f0ffe2e
GreggMays <108717496+GreggMays@users.noreply.github.com>
2025-12-08 20:44:52 +0800

    .

--- main.py
+++ main.py
@@ -114,4 +114,4 @@ def root():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uv.run("main:app", host="0.0.0.0", port=port, reload=True)
+    #uv.run("main:app", host="0.0.0.0", port=port, reload=True)
